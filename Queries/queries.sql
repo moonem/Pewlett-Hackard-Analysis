13a1cdb68a987d34090fa124577dca8c4a289ab0
Moonem <m.a.moonem@gmail.com>
2021-06-20 03:00:30 -0500

    creating a new database PH_EmployeeDB and exporting a table retirement_info

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,1 +1,6 @@
-SELECT * FROM employees
+-- retirement info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
